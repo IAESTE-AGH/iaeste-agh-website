4669fdde7fb4461951cf9978e55212b1d1f335dd
Kacper <kacper.siembab@gmail.com>
2024-11-10 22:11:18 +0100

    finish? Theme interface

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -2,20 +2,60 @@ import type { BoxShadow, Color, LinearGradient } from './color';
 
 export interface Theme {
   colors: {
+    isDark: boolean;
     border: Color;
+    borderRadius: {
+      default: RadiusValue;
+      sm: RadiusValue;
+      md: RadiusValue;
+      lg: RadiusValue;
+    };
+    spacing: {
+      default: SpacingValue;
+      xs: SpacingValue;
+      sm: SpacingValue;
+      md: SpacingValue;
+      lg: SpacingValue;
+      xl: SpacingValue;
+    };
     background: {
       primary: Color;
-      secondary?: Color;
+      secondary: Color;
     };
     text: {
-      primary: Color;
-      secondary?: Color;
+      onLight: Color;
+      onDark: Color;
       link: Color;
     };
     gradients: Record<string, LinearGradient>;
     shadows: Record<string, BoxShadow>;
   };
-  typography: Record<string, FontSettings>;
+  typography: {
+    bold: {
+      display: FontSettings;
+      h1: FontSettings;
+      h2: FontSettings;
+      h3: FontSettings;
+      h4: FontSettings;
+      h5: FontSettings;
+      h6: FontSettings;
+      label: FontSettings;
+      body: FontSettings;
+      link: FontSettings;
+    };
+    regular: {
+      display: FontSettings;
+      h1: FontSettings;
+      h2: FontSettings;
+      h3: FontSettings;
+      h4: FontSettings;
+      h5: FontSettings;
+      h6: FontSettings;
+      label: FontSettings;
+      body: FontSettings;
+      link: FontSettings;
+    };
+  };
 }
 
 interface FontSettings {
@@ -24,3 +64,6 @@ interface FontSettings {
   fontWeight: number;
   lineHeight: number;
 }
+
+type SpacingValue = number | `${number}px`;
+type RadiusValue = number | `${number}px`;
