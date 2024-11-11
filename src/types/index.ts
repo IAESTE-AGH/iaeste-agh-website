1b61a4cecd421a882aba1fa2302aac025b919d02
Kacper <kacper.siembab@gmail.com>
2024-11-11 02:08:46 +0100

    add TextVariant type

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,21 +3,21 @@ import type { Theme } from './theme';
 export type { Theme };
 
 export interface FontSettings {
-  fontFamily: string;
-  fontSize: FontSize;
-  fontWeight: number;
-  lineHeight: LineHeight;
-  textDecoration?: 'none' | 'underline';
+    fontFamily: string;
+    fontSize: FontSize;
+    fontWeight: number;
+    lineHeight: LineHeight;
+    textDecoration?: 'none' | 'underline';
 }
 
 export type FontSize = number | `${number}px` | `${number}rem` | `${number}em`;
 
 export type LineHeight =
-  | number
-  | `${number}px`
-  | `${number}rem`
-  | `${number}em`
-  | 'normal';
+    | number
+    | `${number}px`
+    | `${number}rem`
+    | `${number}em`
+    | 'normal';
 
 export type SpacingValue = number | `${number}px`;
 export type RadiusValue = number | `${number}px`;
@@ -30,9 +30,21 @@ type HEX = `#${string}`;
 export type Color = RGB | RGBA | HEX;
 
 export type LinearGradient =
-  | `linear-gradient(${number}deg, ${Color}, ${Color})`
-  | `linear-gradient(${number}deg, ${Color}, ${Color}, ${Color})`;
+    | `linear-gradient(${number}deg, ${Color}, ${Color})`
+    | `linear-gradient(${number}deg, ${Color}, ${Color}, ${Color})`;
 
 export type BoxShadow =
-  | `${number}px ${number}px ${number}px ${number}px ${Color}`
-  | BoxShadow[];
+    | `${number}px ${number}px ${number}px ${number}px ${Color}`
+    | BoxShadow[];
+
+export type TextVariant =
+    | 'display'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'label'
+    | 'body'
+    | 'link';
