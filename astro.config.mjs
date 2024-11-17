27c5e48dd7b2fa62a5ceff999ddf324047d0de76
Kacper <kacper.siembab@gmail.com>
2024-11-17 13:58:58 +0100

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon({
-      include: {
-        solar: ['*'],
-      },
-    }),
-  ],
+  integrations: [icon({
+    include: {
+      solar: ['*'],
+    },
+  }), react()],
 });
