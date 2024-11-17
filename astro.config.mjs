f4df02f8721f2f4cc704e753747a8a32c5604fa5
Kacper <kacper.siembab@gmail.com>
2024-11-17 15:15:10 +0100

    remove react import [i forgor]

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [
