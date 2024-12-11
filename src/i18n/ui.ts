a33edf6175682cd47a2cae5f6377d07e1110f70e
Kacper <kacper.siembab@gmail.com>
2024-12-11 15:27:47 +0100

    add translation keys for the ui

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,23 +10,47 @@ export const ui = {
         'nav.exchange': 'Wymiana',
         'nav.forCompanies': 'Dla firm',
         'nav.contact': 'Kontakt',
+
+        'section.ourValues': 'Nasze wartości',
+        'section.visitNewPlaces': 'Odwiedź z nami nowe miejsca!',
+
         'values.trust': 'Zaufanie',
         'values.growth': 'Rozwój',
         'values.commitment': 'Aktywność',
         'values.friendship': 'Przyjaźń',
         'values.diversity': 'Różnorodność',
-        'section.ourValues': 'Nasze wartości',
+
+        'newPlaces.happyStudents': 'Zadowolonych studentów',
+        'newPlaces.sentStudents': 'Przyjętych i wysłanych studentów rocznie',
+        'newPlaces.countries': 'Krajów partnerskich',
+        'newPlaces.internships': 'Płatnych praktyk dostępnych co roku',
+        'newPlaces.info.title': 'Chcesz dowiedzieć się więcej o wymianie?',
+        'newPlaces.info.text':
+            'Zapraszamy cię na stronę główną IAESTE Polska, tam dowiesz się wszystkiego o praktykach.',
+        'newPlaces.info.button': 'Odwiedź stronę IAESTE Polska',
     },
     en: {
         'nav.exchange': 'Exchange',
         'nav.forCompanies': 'For companies',
         'nav.contact': 'Contact',
+
+        'section.ourValues': 'Our Values',
+        'section.visitNewPlaces': 'Visit new places with us!',
+
         'values.trust': 'Trust',
         'values.growth': 'Growth',
         'values.commitment': 'Commitment',
         'values.friendship': 'Friendship',
         'values.diversity': 'Diversity',
-        'section.ourValues': 'Our Values',
+
+        'newPlaces.happyStudents': 'Happy students',
+        'newPlaces.sentStudents': 'Students sent and accepted annually',
+        'newPlaces.countries': 'Partner countries',
+        'newPlaces.internships': 'Paid internships available annually',
+        'newPlaces.info.title': 'Want to learn more about exchange?',
+        'newPlaces.info.text':
+            'Visit IAETSE Poland to learn everything about internships.',
+        'newPlaces.info.button': 'Visit IAESTE Poland',
     },
 } as const;
 
