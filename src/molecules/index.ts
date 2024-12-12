98660f41b06028983f3e0408b56a931688293ab8
Kacper <kacper.siembab@gmail.com>
2024-12-12 23:40:38 +0100

    add NewPlacesSection and InfoCardImage components; update InfoCard and ThemedSection for improved layout and styling

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -1,5 +1,6 @@
 import InfoCard from './InfoCard.astro';
 import Navigation from './Navigation.astro';
 import ValueCard from './ValueCard.astro';
+import InfoCardImage from './InfoCardImage.astro';
 
-export { InfoCard, Navigation, ValueCard };
+export { InfoCard, Navigation, ValueCard, InfoCardImage };
