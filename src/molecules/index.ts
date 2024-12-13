7ccfa088a197e6bd38cabefa534d012831a8f303
ShakingDonut <kacper.siembab@gmail.com>
2024-12-13 15:50:44 +0100

    add ShortActionCard component and integrate into TimelineSection

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -2,5 +2,6 @@ import InfoCard from './InfoCard.astro';
 import Navigation from './Navigation.astro';
 import ValueCard from './ValueCard.astro';
 import InfoCardImage from './InfoCardImage.astro';
+import ShortActionCard from './ShortActionCard.astro';
 
-export { InfoCard, Navigation, ValueCard, InfoCardImage };
+export { InfoCard, Navigation, ValueCard, InfoCardImage, ShortActionCard };
