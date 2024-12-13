197581b7e4e0190f934d1fd547c2c8766974ef04
ShakingDonut <kacper.siembab@gmail.com>
2024-12-13 14:19:18 +0100

    quick fix

--- src/organisms/index.ts
+++ src/organisms/index.ts
@@ -1,4 +1,6 @@
 import { Footer } from './Footer';
 import OurValuesSection from './OurValuesSection.astro';
+import NewPlacesSection from './NewPlacesSection.astro';
+import TimelineSection from './TimelineSection.astro';
 
-export { OurValuesSection, Footer };
+export { OurValuesSection, Footer, NewPlacesSection, TimelineSection };
