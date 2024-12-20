eb73febdaccee76a751ea82fcd44bcd09af2e229
ShakingDonut <kacper.siembab@gmail.com>
2024-12-20 20:24:29 +0100

    add timeline section with cards

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -3,5 +3,13 @@ import Navigation from './Navigation.astro';
 import ValueCard from './ValueCard.astro';
 import InfoCardImage from './InfoCardImage.astro';
 import ShortActionCard from './ShortActionCard.astro';
+import SeasonSectionCard from './SeasonSectionCard.astro';
 
-export { InfoCard, Navigation, ValueCard, InfoCardImage, ShortActionCard };
+export {
+    InfoCard,
+    Navigation,
+    ValueCard,
+    InfoCardImage,
+    ShortActionCard,
+    SeasonSectionCard,
+};
